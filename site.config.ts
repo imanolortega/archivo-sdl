395f64c7f4fd60772c4bcded88898fb10c80dd77
imanol <imanolrtega@gmail.com>
2025-07-11 18:52:32 -0300

    Rename site

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_name: "next-wp",
+  site_name: "Archivo SDL",
   site_description: "Somos Subida de Línea",
   site_domain: "https://archivosdl.com",
 };
